353d0048e56ddddf70a5b69860123bcc860dff03
Kynosaur <kye.bracey@gmail.com>
2017-06-07 11:25:04 +0100

    First commit.  Adds initial test for thermostat starting at 20 degrees

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe("Thermostat", function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it("Starts at 20 degrees", function() {
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+});
